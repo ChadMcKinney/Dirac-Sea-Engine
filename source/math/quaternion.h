04b87241cf17bcbf616a8508504bab9e1830b454
Chad McKinney <chad@chadmckinneyaudio.com>
2020-05-23 18:46:13 -0700

    Added quaterion CreateRotationX/Y/Z and cleaned up formatting

--- source/math/quaternion.h
+++ source/math/quaternion.h
@@ -16,61 +16,70 @@
 template <typename T>
 struct Quaternion
 {
-  Quaternion();
-  Quaternion(EZero);
-  Quaternion(EIdentity);
-  Quaternion(EUninitialized);
-  Quaternion(T _x, T _y, T _z, T _w);
+	Quaternion();
+	Quaternion(EZero);
+	Quaternion(EIdentity);
+	Quaternion(EUninitialized);
+	Quaternion(T _x, T _y, T _z, T _w);
 
 	inline bool operator==(const Quaternion& rhs) const;
 	inline bool operator!=(const Quaternion& rhs) const;
 	inline bool IsEquivalent(const Quaternion& rhs, T epsilon) const;
 
-  inline void operator*=(const Quaternion& rhs);
-  inline Quaternion operator*(const Quaternion& rhs) const;
+	inline void operator*=(const Quaternion& rhs);
+	inline Quaternion operator*(const Quaternion& rhs) const;
 
-  inline T Magnitude() const;
-  inline bool IsUnit(T epsilon) const;
+	inline T Magnitude() const;
+	inline bool IsUnit(T epsilon) const;
 
-  inline void Negate();
-  inline Quaternion<T> Negated() const;
+	inline void Negate();
+	inline Quaternion Negated() const;
 
-  inline void Invert(); // Assumes unit quaternion!
-  inline Quaternion Inverted() const; // Assumes unit quaternion!
+	inline void Invert(); // Assumes unit quaternion!
+	inline Quaternion Inverted() const; // Assumes unit quaternion!
 
-  inline void SetAxisAngle(const Vec3<T>& axis, T radians);
-  inline static Quaternion<T> CreateAxisAngle(const Vec3<T> axis, T radians);
+	inline void SetAxisAngle(const Vec3<T>& axis, T radians);
+	inline static Quaternion CreateAxisAngle(const Vec3<T> axis, T radians);
 
-  T x, y, z, w;
+	inline void SetRotationX(T radians);
+	inline static Quaternion CreateRotationX(T radians);
+
+	inline void SetRotationY(T radians);
+	inline static Quaternion CreateRotationY(T radians);
+
+	inline void SetRotationZ(T radians);
+	inline static Quaternion CreateRotationZ(T radians);
+
+	T x, y, z, w;
 };
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 Quaternion<T>::Quaternion()
-  : x(T{})
-  , y(T{})
-  , z(T{})
-  , w(T{})
+	: x(T{})
+	, y(T{})
+	, z(T{})
+	, w(T{})
 {
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 Quaternion<T>::Quaternion(EZero)
-  : x(0)
-  , y(0)
-  , z(0)
-  , w(0)
+	: x(0)
+	, y(0)
+	, z(0)
+	, w(0)
 {
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 Quaternion<T>::Quaternion(EIdentity)
-  : x(0)
-  , y(0)
-  , z(0)
-  , w(1)
+	: x(0)
+	, y(0)
+	, z(0)
+	, w(1)
 {
 }
 
@@ -83,10 +92,10 @@ Quaternion<T>::Quaternion(EUninitialized)
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 Quaternion<T>::Quaternion(T _x, T _y, T _z, T _w)
-  : x(_x)
-  , y(_y)
-  , z(_z)
-  , w(_w)
+	: x(_x)
+	, y(_y)
+	, z(_z)
+	, w(_w)
 {
 }
 
@@ -94,115 +103,178 @@ Quaternion<T>::Quaternion(T _x, T _y, T _z, T _w)
 template <typename T>
 inline bool Quaternion<T>::operator==(const Quaternion& rhs) const
 {
-  return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
+	return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline bool Quaternion<T>::operator!=(const Quaternion& rhs) const
 {
-  return x != rhs.x || y != rhs.y || z != rhs.z || w != rhs.w;
+	return x != rhs.x || y != rhs.y || z != rhs.z || w != rhs.w;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline bool Quaternion<T>::IsEquivalent(const Quaternion& rhs, T epsilon) const
 {
-  return
-    (abs(x - rhs.x) < epsilon) &&
-    (abs(y - rhs.y) < epsilon) &&
-    (abs(z - rhs.z) < epsilon) &&
-    (abs(w - rhs.w) < epsilon);
+	return
+		(abs(x - rhs.x) < epsilon) &&
+		(abs(y - rhs.y) < epsilon) &&
+		(abs(z - rhs.z) < epsilon) &&
+		(abs(w - rhs.w) < epsilon);
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline T Quaternion<T>::Magnitude() const
 {
-  return sqrt((x * x) + (y * y) + (z * z) + (w * w));
+	return sqrt((x * x) + (y * y) + (z * z) + (w * w));
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline void Quaternion<T>::operator*=(const Quaternion& rhs)
 {
-  *this = *this * rhs;
+	*this = *this * rhs;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline Quaternion<T> Quaternion<T>::operator*(const Quaternion& rhs) const
 {
-  Quaternion<T> q(EUninitialized::Constructor);
-  q.x = (w * rhs.x) + (x * rhs.w) + (y * rhs.z) - (z * rhs.y);
-  q.y = (w * rhs.y) + (y * rhs.w) + (z * rhs.x) - (x * rhs.z);
-  q.z = (w * rhs.z) + (z * rhs.w) + (x * rhs.y) - (y * rhs.x);
-  q.w = (w * rhs.w) - (x * rhs.x) - (y * rhs.y) - (z * rhs.z);
-  return q;
+	Quaternion<T> q(EUninitialized::Constructor);
+	q.x = (w * rhs.x) + (x * rhs.w) + (y * rhs.z) - (z * rhs.y);
+	q.y = (w * rhs.y) + (y * rhs.w) + (z * rhs.x) - (x * rhs.z);
+	q.z = (w * rhs.z) + (z * rhs.w) + (x * rhs.y) - (y * rhs.x);
+	q.w = (w * rhs.w) - (x * rhs.x) - (y * rhs.y) - (z * rhs.z);
+	return q;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline bool Quaternion<T>::IsUnit(T epsilon) const
 {
-  return (T(1) - Magnitude()) < epsilon;
+	return (T(1) - Magnitude()) < epsilon;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline void Quaternion<T>::Negate()
 {
-  x = -x;
-  y = -y;
-  z = -z;
-  w = -w;
+	x = -x;
+	y = -y;
+	z = -z;
+	w = -w;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline Quaternion<T> Quaternion<T>::Negated() const
 {
-  return Quaternion<T>(-x, -y, -z, -w);
+	return Quaternion<T>(-x, -y, -z, -w);
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline void Quaternion<T>::Invert()
 {
-  assert(IsUnit(epsilon<T>()));
-  x = -x;
-  y = -y;
-  z = -z;
+	assert(IsUnit(epsilon<T>()));
+	x = -x;
+	y = -y;
+	z = -z;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline Quaternion<T> Quaternion<T>::Inverted() const
 {
-  assert(IsUnit(epsilon<T>()));
-  return Quaternion<T>(-x, -y, -z, w);
+	assert(IsUnit(epsilon<T>()));
+	return Quaternion<T>(-x, -y, -z, w);
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline void Quaternion<T>::SetAxisAngle(const Vec3<T>& axis, T radians)
 {
-  assert(axis.IsUnit(epsilon<T>()));
-  T halfTheta = radians * T(0.5);
-  T sinHalfTheta = std::sin(halfTheta);
-  w = std::cos(halfTheta);
-  x = axis.x * sinHalfTheta;
-  y = axis.y * sinHalfTheta;
-  z = axis.z * sinHalfTheta;
+	assert(axis.IsUnit(epsilon<T>()));
+	const T halfTheta = radians * T(0.5);
+	const T sinHalfTheta = std::sin(halfTheta);
+	w = std::cos(halfTheta);
+	x = axis.x * sinHalfTheta;
+	y = axis.y * sinHalfTheta;
+	z = axis.z * sinHalfTheta;
 }
 
 ///////////////////////////////////////////////////////////////////////
 template <typename T>
 inline Quaternion<T> Quaternion<T>::CreateAxisAngle(const Vec3<T> axis, T radians)
 {
-  Quaternion<T> q(EUninitialized::Constructor);
-  q.SetAxisAngle(axis, radians);
-  return q;
+	Quaternion<T> q(EUninitialized::Constructor);
+	q.SetAxisAngle(axis, radians);
+	return q;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline void Quaternion<T>::SetRotationX(T radians)
+{
+	const T halfTheta = radians * T(0.5);
+	const T sinHalfTheta = std::sin(halfTheta);
+	w = std::cos(halfTheta);
+	x = sinHalfTheta;
+	y = 0;
+	z = 0;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline Quaternion<T> Quaternion<T>::CreateRotationX(T radians)
+{
+	Quaternion<T> q(EUninitialized::Constructor);
+	q.SetRotationX(radians);
+	return q;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline void Quaternion<T>::SetRotationY(T radians)
+{
+	const T halfTheta = radians * T(0.5);
+	const T sinHalfTheta = std::sin(halfTheta);
+	w = std::cos(halfTheta);
+	x = 0;
+	y = sinHalfTheta;
+	z = 0;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline Quaternion<T> Quaternion<T>::CreateRotationY(T radians)
+{
+	Quaternion<T> q(EUninitialized::Constructor);
+	q.SetRotationY(radians);
+	return q;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline void Quaternion<T>::SetRotationZ(T radians)
+{
+	const T halfTheta = radians * T(0.5);
+	const T sinHalfTheta = std::sin(halfTheta);
+	w = std::cos(halfTheta);
+	x = 0;
+	y = 0;
+	z = sinHalfTheta;
+}
+
+///////////////////////////////////////////////////////////////////////
+template <typename T>
+inline Quaternion<T> Quaternion<T>::CreateRotationZ(T radians)
+{
+	Quaternion<T> q(EUninitialized::Constructor);
+	q.SetRotationZ(radians);
+	return q;
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -210,9 +282,9 @@ inline Quaternion<T> Quaternion<T>::CreateAxisAngle(const Vec3<T> axis, T radian
 template <typename T>
 inline Vec3<T> operator*(const Quaternion<T>& q, const Vec3<T>& v)
 {
-  const Quaternion<T> qv(v.x, v.y, v.z, 0);
-  const Quaternion<T> qv2 = q * qv * q.Inverted();
-  return Vec3<T>(qv2.x, qv2.y, qv2.z);
+	const Quaternion<T> qv(v.x, v.y, v.z, 0);
+	const Quaternion<T> qv2 = q * qv * q.Inverted();
+	return Vec3<T>(qv2.x, qv2.y, qv2.z);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -220,10 +292,10 @@ inline Vec3<T> operator*(const Quaternion<T>& q, const Vec3<T>& v)
 template <typename T>
 inline Vec3<T> operator*(const Vec3<T>& v, const Quaternion<T>& q)
 {
-  const Quaternion<T> q2 = q.Inverted();
-  const Quaternion<T> qv(v.x, v.y, v.z, 0);
-  const Quaternion<T> qv2 = q2 * qv * q;
-  return Vec3<T>(qv2.x, qv2.y, qv2.z);
+	const Quaternion<T> q2 = q.Inverted();
+	const Quaternion<T> qv(v.x, v.y, v.z, 0);
+	const Quaternion<T> qv2 = q2 * qv * q;
+	return Vec3<T>(qv2.x, qv2.y, qv2.z);
 }
 
 ///////////////////////////////////////////////////////////////////////
--- source/tests/math/quaternion/quaternion_tests.cpp
+++ source/tests/math/quaternion/quaternion_tests.cpp
@@ -11,82 +11,103 @@
 template <typename T>
 void RunQuaternionTest_tpl()
 {
-  {
-    Quaternion<T> q(EZero::Constructor);
-    Quaternion<T> q2(EZero::Constructor);
-    TEST("quaternion: ZERO == ZERO", q == q2);
-  }
+	{
+		Quaternion<T> q(EZero::Constructor);
+		Quaternion<T> q2(EZero::Constructor);
+		TEST("quaternion: ZERO == ZERO", q == q2);
+	}
 
-  {
-    Quaternion<T> q(EIdentity::Constructor);
-    Quaternion<T> q2(EIdentity::Constructor);
-    TEST("quaternion: IDENTITY == IDENTITY", q == q2);
-  }
+	{
+		Quaternion<T> q(EIdentity::Constructor);
+		Quaternion<T> q2(EIdentity::Constructor);
+		TEST("quaternion: IDENTITY == IDENTITY", q == q2);
+	}
 
-  {
-    Quaternion<T> q(EIdentity::Constructor);
-    Quaternion<T> q2(0, 0, 0, 1 - epsilon<T>());
-    TEST("quaternion: IDENTITY == IDENTITY - epsilon", q.IsEquivalent(q2, epsilon<T>() * 2));
-  }
+	{
+		Quaternion<T> q(EIdentity::Constructor);
+		Quaternion<T> q2(0, 0, 0, 1 - epsilon<T>());
+		TEST("quaternion: IDENTITY == IDENTITY - epsilon", q.IsEquivalent(q2, epsilon<T>() * 2));
+	}
 
-  {
-    Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>(1, 0, 0), 0);
-    Quaternion<T> q2(EIdentity::Constructor);
-    TEST("quaternion: IDENTITY == AA_ZERO", q == q2);
-  }
+	{
+		Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>(1, 0, 0), 0);
+		Quaternion<T> q2(EIdentity::Constructor);
+		TEST("quaternion: IDENTITY == AA_ZERO", q == q2);
+	}
 
-  {
-    Quaternion<T> q(EIdentity::Constructor);
-    Quaternion<T> q2 = q * q;
-    TEST("quaternion: IDENTITY * IDENTITY == IDENTITY", q2 == q);
-  }
+	{
+		Quaternion<T> q(EIdentity::Constructor);
+		Quaternion<T> q2 = q * q;
+		TEST("quaternion: IDENTITY * IDENTITY == IDENTITY", q2 == q);
+	}
 
-  {
-    Quaternion<T> q(EIdentity::Constructor);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = q * v;
-    TEST("quaternion:: IDENTITY * v == v", v == v2);
-  }
+	{
+		Quaternion<T> q(EIdentity::Constructor);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * v;
+		TEST("quaternion:: IDENTITY * v == v", v == v2);
+	}
 
-  {
-    Quaternion<T> q(EIdentity::Constructor);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = v * q;
-    TEST("quaternion:: v * IDENTITY == v", v == v2);
-  }
+	{
+		Quaternion<T> q(EIdentity::Constructor);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = v * q;
+		TEST("quaternion:: v * IDENTITY == v", v == v2);
+	}
 
-  {
-    Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kPi<T>);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = q * v;
-    TEST("quaternion:: AA * v", v2.IsEquivalent(Vec3<T>::Down, epsilon<T>()));
-  }
+	{
+		Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * v;
+		TEST("quaternion:: AA * v", v2.IsEquivalent(Vec3<T>::Down, epsilon<T>()));
+	}
 
-  {
-    Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kPi<T>);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = q * q * v;
-    TEST("quaternion:: AA * AA * v", v2.IsEquivalent(Vec3<T>::Up, epsilon<T>() * 2));
-  }
+	{
+		Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * q * v;
+		TEST("quaternion:: AA * AA * v", v2.IsEquivalent(Vec3<T>::Up, epsilon<T>() * 2));
+	}
 
-  {
-    Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kHalfPi<T>);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = q.Inverted() * q * v;
-    TEST("quaternion:: AA.Inverted() * AA * v", v2.IsEquivalent(Vec3<T>::Up, epsilon<T>() * 4));
-  }
+	{
+		Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kHalfPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q.Inverted() * q * v;
+		TEST("quaternion:: AA.Inverted() * AA * v", v2.IsEquivalent(Vec3<T>::Up, epsilon<T>() * 4));
+	}
 
-  {
-    Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kHalfPi<T>);
-    Vec3<T> v = Vec3<T>::Up;
-    Vec3<T> v2 = q * v;
-    Vec3<T> v3 = v * q.Inverted();
-    TEST("quaternion:: AA * v == v * AA.Inverted()", v2.IsEquivalent(v3, epsilon<T>() * 4));
-  }
+	{
+		Quaternion<T> q = Quaternion<T>::CreateAxisAngle(Vec3<T>::Forward, kHalfPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * v;
+		Vec3<T> v3 = v * q.Inverted();
+		TEST("quaternion:: AA * v == v * AA.Inverted()", v2.IsEquivalent(v3, epsilon<T>() * 4));
+	}
+
+	{
+		Quaternion<T> q = Quaternion<T>::CreateRotationX(kPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * v;
+		TEST("quaternion:: RotationX * v", v2.IsEquivalent(Vec3<T>::Down, epsilon<T>()));
+	}
+
+	{
+		Quaternion<T> q = Quaternion<T>::CreateRotationY(kPi<T>);
+		Vec3<T> v = Vec3<T>::Forward;
+		Vec3<T> v2 = q * v;
+		TEST("quaternion:: RotationY * v", v2.IsEquivalent(Vec3<T>::Back, epsilon<T>()));
+	}
+
+	{
+		Quaternion<T> q = Quaternion<T>::CreateRotationZ(kPi<T>);
+		Vec3<T> v = Vec3<T>::Up;
+		Vec3<T> v2 = q * v;
+		TEST("quaternion:: RotationZ * v", v2.IsEquivalent(Vec3<T>::Down, epsilon<T>()));
+	}
 }
 
 void RunQuaternionTests()
 {
-  RunQuaternionTest_tpl<flocal>();
-  RunQuaternionTest_tpl<fworld>();
+	RunQuaternionTest_tpl<flocal>();
+	RunQuaternionTest_tpl<fworld>();
 }
